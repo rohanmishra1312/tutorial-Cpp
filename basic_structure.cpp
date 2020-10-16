355dddb979ee67e95425d6ffef75e47aa68eb749
Rohan Mishra <mishra.rohan1312@gmail.com>
2020-10-16 13:10:45 +0530

    First Commit

---
+++ basic_structure.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int ageOfUser;
+   cout << "Hello User" << endl;
+   cout << "Welcome to Basics of CPP. Please Enter Your Age" << endl;
+   cin >> ageOfUser;
+   cout <<"Your Age is: " << ageOfUser <<endl; 
+
+   return 0;
+}  
